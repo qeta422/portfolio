cd71e2b20e0680565e841f1c972ca45ba530a517
qeta422 <qetafetviashvili123@gmail.com>
2024-02-11 17:54:06 +0400

    typewriter animated text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var nav = document.getElementsByClassName("nav")[0];
+var typeWriter = document.getElementsByClassName("typewriter-text")[0];
 
 window.addEventListener("scroll", () => {
     nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", () => {
+    typeWriter.classList.toggle("animated", window.scrollY > 0);
 });
