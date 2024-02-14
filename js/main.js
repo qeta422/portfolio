70cd9d087c27f5fcec507769cb6ed58f97ff9446
qeta422 <qetafetviashvili123@gmail.com>
2024-02-15 01:19:50 +0400

    slide animation change

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var nav = document.getElementsByClassName("nav")[0];
 var typeWriter = document.getElementsByClassName("typewriter-text")[0];
-var tree = document.getElementsByClassName("tree-item-first")[0];
-var second = document.getElementsByClassName("tree-item-second")[0];
-var third = document.getElementsByClassName("tree-item-third")[0];
-var fourth = document.getElementsByClassName("tree-item-fourth")[0];
-var fifth = document.getElementsByClassName("tree-item-fifth")[0];
+var tree = document.getElementsByClassName("first")[0];
+var second = document.getElementsByClassName("second")[0];
+var third = document.getElementsByClassName("third")[0];
+var fourth = document.getElementsByClassName("fourth")[0];
+var fifth = document.getElementsByClassName("fifth")[0];
 
 window.addEventListener("scroll", () => {
     nav.classList.toggle("sticky", window.scrollY > 0);
